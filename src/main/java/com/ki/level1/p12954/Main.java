eb7e21444925ee7653593fcef91bd6cd60902341
qBlackbirdp <blackbird9711@gmail.com>
2024-07-26 09:17:31 +0900

    타입 수정 숫자 범위가 문제였음..

--- src/main/java/com/ki/level1/p12954/Main.java
+++ src/main/java/com/ki/level1/p12954/Main.java
@@ -7,15 +7,15 @@ public class Main {
 }
 
 class Solution {
-    public int[] solution(int x, int n) {
+    public long[] solution(int x, int n) {
         if (n == 0) {
-            return new int[0];
+            return new long[0];
         }
 
-        int[] answer = new int[n];
+        long[] answer = new long[n];
 
         for (int i = 0; i < n; i++) {
-            answer[i] = x * (i + 1);
+            answer[i] =(long) x * (i + 1);
         }
 
         return answer;
--- src/test/java/com/ki/level1/p12954/Tests.java
+++ src/test/java/com/ki/level1/p12954/Tests.java
@@ -9,24 +9,24 @@ public class Tests {
     @Test
     @DisplayName("값 2,4,6,8,10")
     void t1() {
-        assertThat(new Solution().solution(2, 5)).isEqualTo(new int[]{2,4,6,8,10});
+        assertThat(new Solution().solution(2, 5)).isEqualTo(new long[]{2,4,6,8,10});
     }
 
     @Test
     @DisplayName("값 4, 8, 12")
     void t2() {
-        assertThat(new Solution().solution(4, 3)).isEqualTo(new int[]{4, 8, 12});
+        assertThat(new Solution().solution(4, 3)).isEqualTo(new long[]{4, 8, 12});
     }
 
     @Test
     @DisplayName("값 -4, -8")
     void t3() {
-        assertThat(new Solution().solution(-4, 2)).isEqualTo(new int[]{-4, -8});
+        assertThat(new Solution().solution(-4, 2)).isEqualTo(new long[]{-4, -8});
     }
 
     @Test
     @DisplayName("값 0, 0")
     void t4() {
-        assertThat(new Solution().solution(0, 2)).isEqualTo(new int[]{0, 0});
+        assertThat(new Solution().solution(0, 2)).isEqualTo(new long[]{0, 0});
     }
 }
